f998b128b2b0fc1b3d1c0241e185f5111f7488a3
phyosithuaung <mr.phyosithuaung@gmail.com>
2023-09-22 22:19:46 +0630

    add button color feature

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const seconds = document.querySelector(".seconds");
 const minutes = document.querySelector(".minutes");
 const hours = document.querySelector(".hours");
 const count = document.querySelector(".count");
+
+
+
 let sec = 0 ,min = 0, hur = 0 ,mili = 0;
 
 const startTime = () =>{
@@ -26,32 +29,67 @@ const startTime = () =>{
     
 
 }
+
+
+
+
 let intervalId;
 startBtn.addEventListener("click",()=> {
+    if (sec == 0 & min == 0 & hur == 0 & mili == 0){
     clearInterval(intervalId);
     intervalId = setInterval(startTime, 10);
+    btnOn();
+    return;
+    }
+    btnOff();
+    clearInterval(intervalId);
+    intervalId = setInterval(startTime, 10);
+    btnOn();
+    
+
+    
+    
+    
+
     
 });
 
 pauseBtn.addEventListener("click",() => {
+    if (sec == 0 & min == 0 & hur == 0 & mili == 0) {
+        return;
+    };
+    btnOff();
     clearInterval(intervalId);
+    btnOn();
 
+
+    
 });
 continueBtn.addEventListener("click",()=> {
+    if (sec == 0 & min == 0 & hur == 0 & mili == 0) {
+       return;
+   }
+    btnOff();
     clearInterval(intervalId);
     intervalId = setInterval(startTime, 10);
+    btnOn();
 });
 resetBtn.addEventListener("click", ()=> {
-     
+    if (sec == 0 & min == 0 & hur == 0 & mili == 0) {
+        return;
+    }
+     btnOff();
     sec = 0;
     min = 0;
     hur = 0;
     mili = 0;
     display();
     clearInterval(intervalId);
-    
-    
-})
+    btnOn();
+    setTimeout(()=> {resetBtn.style.background = "#FFFFFF";
+    resetBtn.style.color = "#047ac9"},1000)
+    return;
+});
 
 const display = ()=>{
     const secText  = sec < 10 ? "0"+ sec+"" : sec;
@@ -63,4 +101,20 @@ const display = ()=>{
     minutes.textContent = minText;
     hours.textContent = hurText;
     count.textContent = miliText;
+}
+const btnOff = () =>{
+    const isOpen = document.getElementsByClassName("isopen")[0];
+    isOpen.style.background = "#FFFFFF";
+    isOpen.style.color = "#047ac9";
+    isOpen.classList.remove("isopen");
+}
+
+const btnOn = () =>{
+    const clickId = event.target.id;
+    let clickbtn = document.getElementsByClassName(clickId)[0];
+    clickbtn.style.background = "#047ac9";
+    clickbtn.style.color = "#FFFFFF";
+    console.log(clickbtn);
+    clickbtn.classList.add("isopen");
+
 }
